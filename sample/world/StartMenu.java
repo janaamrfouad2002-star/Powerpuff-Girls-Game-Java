857b8a4b842fc1923c4e861c97510946d8c30e65
jana amr <janaamrfouad2002@gmail.com>
2026-06-20 01:19:57 +0300

    Add files via upload

---
+++ sample/world/StartMenu.java
@@ -0,0 +1,17 @@
+
+package eg.edu.alexu.csd.oop.game.sample.world;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+public class StartMenu implements ActionListener {
+    private JFrame frame;
+    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
